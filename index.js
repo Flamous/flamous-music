0e60adce39e6eb09e65acb32f49df881c0af390e
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-07-08 22:04:55 +0200

    Playin' around with Hyperapp

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-import {h, app} from "hyperapp"
+import { h, app } from 'hyperapp'
 
+const state = {
+  pages: [
+  ]
+}
 
-const state = {}
+const actions = {
+  addPage: () => ({pages}) => ({
+    pages: (pages.push({id: pages.length}), pages)
+  }),
+  removePage: () => ({pages}) => ({
+    pages: (pages.pop(), pages)
+  })
+}
 
-const actions = {}
+const Page = ({pageNumber: id, addPage, removePage}) =>
+  h('div', { class: 'page-container' }, [
+    h('h1', {}, 'pageNumber'),
+    h('button', {onclick: addPage}, '+ Add Page'),
+    h('button', {onclick: removePage}, '- Remove Page')
+  ])
 
-const view = () =>
-    h("h1", {}, "THIS IS WORKING")
-
+const view = ({pages}, {addPage, removePage}) =>
+  h('div', {}, [
+    h('h1', {}, 'Lowest level'),
+    h('button', {onclick: addPage}, '+ Add Page'),
+    // console.log(state)
+    pages.map(({id}) => (
+      Page({id, addPage, removePage})
+    ))
+  ])
 
 app(state, actions, view, document.body)
