0427343f33d4b52f1180e063fdd7a09cf12e9cac
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-09-19 20:16:09 +0200

    ImagViewer: Try to fix scaling values not being applied

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -31,7 +31,7 @@ function start (data) {
 
   let handleStyler = styler(data.element)
 
-  let handleScale = value({scale: scale}, handleStyler.set)
+  let handleScale = value(scale, handleStyler.set('scale'))
   let handleY = value(invert, handleStyler.set('y'))
 
   data.element.style.transformOrigin = 'top'
@@ -63,7 +63,7 @@ function start (data) {
       multitouch({scale: handleScale.get()})
         .pipe(({scale}) => scale)
         .start({
-          update: handleScale.set,
+          update: handleScale,
           complete: () => {
             let scale = handleScale.get()
             console.log(scale)
