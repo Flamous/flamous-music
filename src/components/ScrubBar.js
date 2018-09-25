9d7c980a7f2b1c7257da9fa3e0bcf178ac5d06ae
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-09-25 09:55:12 +0200

    ScrubBar: First draft of new styles

--- src/components/ScrubBar.js
+++ src/components/ScrubBar.js
@@ -133,14 +133,14 @@ function makeInteractive (element) {
 
 const style = picostyle(h)
 
-const Bubble = style('div')((props) => ({
-  backgroundColor: '#f9f9f9',
-  transition: 'border-color 150ms',
-  borderRadius: '16px',
-  border: props.playingState ? '1px solid #007AFF' : '1px solid #dedede',
+const BubbleStyles = style('div')((props) => ({
+  backgroundColor: '#fdfdfd',
+  // transition: 'border-color 150ms',
+  borderRadius: '3px 3px 0px 0px',
+  // border: props.playingState ? '1px solid #007AFF' : '1px solid #dedede',
   width: '100%',
   maxWidth: '400px',
-  padding: '0.3em',
+  padding: '0.4em',
   display: 'flex',
   position: 'relative',
   height: '4.2em',
@@ -151,27 +151,29 @@ const Bubble = style('div')((props) => ({
   }
 }))
 
-const Indicator = () => style('div')({
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-  position: 'absolute',
-  left: '0px',
-  top: '0px',
-  marginTop: '0.4em'
-})(
-  {class: 'indicator', id: 'indicator'},
-  style('div')({
-    height: '4px',
-    width: '2.1em',
-    backgroundColor: '#CCC',
-    borderRadius: '100px',
-    transition: 'background-color 150ms'
-  })
-)
+const Bubble = BubbleStyles
+
+// const Indicator = () => style('div')({
+//   width: '100%',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   position: 'absolute',
+//   left: '0px',
+//   top: '0px',
+//   marginTop: '0.4em'
+// })(
+//   {class: 'indicator', id: 'indicator'},
+//   style('div')({
+//     height: '4px',
+//     width: '2.1em',
+//     backgroundColor: '#CCC',
+//     borderRadius: '100px',
+//     transition: 'background-color 150ms'
+//   })
+// )
 const SongCover = style('img')({
   height: '100%',
-  borderRadius: '14px',
+  borderRadius: '16%',
   boxSizing: 'border-box',
   pointerEvents: 'none'
 })
@@ -195,7 +197,7 @@ const Wrapper = style('div')({
   display: 'flex',
   position: 'absolute',
   bottom: '0px',
-  padding: '1em 0.5em',
+  // padding: '1em 0.5em',
   boxSizing: 'border-box',
   justifyContent: 'center'
 })
@@ -203,7 +205,7 @@ const Wrapper = style('div')({
 const ScrubBar = (props) =>
   <Wrapper key={props.key}>
     <Bubble playingState={props.playingState} oncreate={makeInteractive}>
-      <Indicator class='indicator' />
+      {/* <Indicator class='indicator' /> */}
       <SongCover draggable='false' src={props.image} />
       <Info>
         <Song>
