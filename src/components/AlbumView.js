fb4d6af2f7a0411fb917384cb6e623bd3f9aeb3c
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-11 20:20:36 +0200

    AlbumView: Add song list and correct title

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -3,9 +3,24 @@ import picostyle from 'picostyle'
 import Page from './Page'
 import Header, { HeaderBold, HeaderImage } from './Header'
 import { Route, Link } from '@hyperapp/router'
+import kimikoSongs from '../songs/kimiko_ishizaka'
+import SongList from './SongList'
 
 const Album = (props) => {
-  return <Header title='So Awesome' />
+  console.log(props.match.params)
+  let title
+  let songs
+  switch (props.match.params.albumId) {
+    case 'open_goldberg_variations':
+      title = 'Goldberg Variations'
+      songs = kimikoSongs.filter((song) => {
+        return song.album === 'J​.​S. Bach: "Open" Goldberg Variations, BWV 988 (Piano)'
+      })
+  }
+  return <div>
+    <Header title={title} />
+    <SongList songs={songs} />
+  </div>
 }
 
 export default (props) => {
