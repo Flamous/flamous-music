c34a341edc8ac62603cb8d8af7cb3e6a5c0b9111
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-11 20:14:20 +0200

    Put SongList in it's own component

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -4,7 +4,7 @@ import Page from './Page'
 import Header, { HeaderBold, HeaderImage } from './Header'
 import { Route, Link } from '@hyperapp/router'
 import LazyLoad from 'vanilla-lazyload'
-import LazyImage from './LazyImage'
+import SongList from './SongList'
 // import profilePic from '../assets/wowa.jpg'
 import playImage from '../public/play_white.svg'
 import artists from '../artists'
@@ -60,69 +60,6 @@ const PlayAllButton = (props) => {
   </ShuffleButtonStyle>
 }
 
-const SongListStyle = style('div')({
-  transition: 'opacity 70ms',
-  ' ul': {
-    listStyleType: 'none',
-    padding: '0'
-  },
-  ' .song-list-item': {
-    display: 'flex',
-    padding: '0.6em 1em 0.6em 1.5em',
-    alignItems: 'center'
-  },
-  ' .song-list-item *:not(img)': {
-    margin: '0'
-  }
-})
-
-const Thumbnail = style(LazyImage)({
-  height: '3.3em',
-  width: '3.3em',
-  marginRight: '0.8em',
-  transition: 'opacity 350ms 300ms',
-  ':not([src])': {
-    opacity: '0'
-  }
-})
-
-const StyledListItem = style('li')({
-  transition: 'background-color 100ms',
-  '&:hover': {
-    backgroundColor: '#fafafa'
-  },
-  '&:active': {
-    backgroundColor: '#f0f0f0',
-    transform: 'scale(0.95)',
-    opacity: '0.7'
-  }
-})
-
-const ListItem = (props) => {
-  return <StyledListItem {...props} class='song-list-item'>
-    <Thumbnail src={props.image} />
-    <div>
-      <p style={{fontWeight: 'bold'}}>{props.title}</p>
-      <p>{props.sub}</p>
-    </div>
-  </StyledListItem>
-}
-
-const SongList = (props) => (context) => {
-  return <SongListStyle>
-    <ul>
-      {
-        props.songs.map((song, index) => {
-          return <ListItem onclick={() => {
-            window.Amplitude.getShuffle() && window.Amplitude.setShuffle(false)
-            window.Amplitude.playSongAtIndex(index)
-            context.actions.scrubBar.show()
-          }} key={song.id} title={song.name} image={song.cover_art_url} sub={song.artist} />
-        })
-      }
-    </ul>
-  </SongListStyle>
-}
 const StyledAlbumThumbnail = style(Link)({
   width: '45%',
   padding: '1.5em'
---
+++ src/components/SongList.js
@@ -0,0 +1,71 @@
+import { h } from 'hyperapp'
+import picostyle from 'picostyle'
+import LazyImage from './LazyImage'
+
+const style = picostyle(h)
+
+const SongListStyle = style('div')({
+  transition: 'opacity 70ms',
+  ' ul': {
+    listStyleType: 'none',
+    padding: '0'
+  },
+  ' .song-list-item': {
+    display: 'flex',
+    padding: '0.6em 1em 0.6em 1.5em',
+    alignItems: 'center'
+  },
+  ' .song-list-item *:not(img)': {
+    margin: '0'
+  }
+})
+
+const Thumbnail = style(LazyImage)({
+  height: '3.3em',
+  width: '3.3em',
+  marginRight: '0.8em',
+  transition: 'opacity 350ms 300ms',
+  ':not([src])': {
+    opacity: '0'
+  }
+})
+
+const StyledListItem = style('li')({
+  transition: 'background-color 100ms',
+  '&:hover': {
+    backgroundColor: '#fafafa'
+  },
+  '&:active': {
+    backgroundColor: '#f0f0f0',
+    transform: 'scale(0.95)',
+    opacity: '0.7'
+  }
+})
+
+const ListItem = (props) => {
+  return <StyledListItem {...props} class='song-list-item'>
+    <Thumbnail src={props.image} />
+    <div>
+      <p style={{fontWeight: 'bold'}}>{props.title}</p>
+      <p>{props.sub}</p>
+    </div>
+  </StyledListItem>
+}
+
+const SongList = (props) => (context) => {
+  return <SongListStyle>
+    <ul>
+      {
+        props.songs.map((song, index) => {
+          return <ListItem onclick={() => {
+            window.Amplitude.getShuffle() && window.Amplitude.setShuffle(false)
+            window.Amplitude.playSongAtIndex(index)
+            context.actions.scrubBar.show()
+          }} key={song.id} title={song.name} image={song.cover_art_url} sub={song.artist} />
+        })
+      }
+    </ul>
+  </SongListStyle>
+}
+
+export default SongList
