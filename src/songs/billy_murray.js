6e4ac42e58ec3c8ad5921b531f3ac1661ca2df17
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-13 19:19:53 +0200

    Fix names

--- src/songs/billy_murray.js
+++ src/songs/billy_murray.js
@@ -3,8 +3,8 @@ import songs from '../public/songs/billy_murray/*.aac'
 
 export default [{
   id: 0,
-  name: 'Aria',
-  artist: "I'll Do It All Over Again",
+  name: "I'll Do It All Over Again",
+  artist: 'Billy Murray',
   // album: 'J​.​S. Bach: "Open" Goldberg Variations, BWV 988 (Piano)',
   url: songs[0],
   cover_art_url: images[0]
