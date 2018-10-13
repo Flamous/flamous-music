ce8e9a63e4d8c50f7e46d4bb34906fb048d7baaf
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-13 11:54:09 +0200

    Header: Hopefully fix backdrop-filter (only effective on Safari devices)

--- src/components/Header.js
+++ src/components/Header.js
@@ -63,7 +63,7 @@ const StyledHeaderNav = style('div')({
   position: 'sticky',
   top: '0px',
   backgroundColor: 'rgba(253, 253, 253, 0.95)',
-  '@supports (backdrop-filter: blur(30px))': {
+  '@supports (-webkit-backdrop-filter: blur(30px)) or (backdrop-filter: blur(30px))': {
     backgroundColor: 'rgba(253, 253, 253, 0.6)',
     backdropFilter: 'blur(30px)',
     WebkitBackdropFilter: 'blur(30px)'
