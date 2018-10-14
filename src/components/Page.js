9c0c570e78e688c0522b2284fabebdc9a8eeb3e4
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-14 18:58:37 +0200

    Page.js: Remove old code

--- src/components/Page.js
+++ src/components/Page.js
@@ -7,115 +7,6 @@ const { snap } = transform
 
 const style = picostyle(h)
 
-// function makeInteractive (element) {
-//   let handleStyler
-//   let handleX
-//   element.style.transform = 'translateX(100%)'
-
-//   const AXIS_LOCK_THRESHOLD = 15
-//   let isAxisLocked = false
-//   handleStyler = styler(element)
-//   handleX = value('0%', handleStyler.set('x'))
-
-//   element.handleX = handleX
-
-//   const pointerX = (preventDefault = false, x = 0) => pointer({x: x, preventDefault: preventDefault}).pipe(val => val.x)
-
-//   // Initial slide-in
-//   spring({
-//     from: '100%',
-//     to: '0%',
-//     damping: 20,
-//     mass: 0.5
-//   }).start(handleX)
-
-//   // Swipe-back mechanism
-//   listen(element, 'mousedown touchstart')
-//     .start((e) => {
-//       let currentPointer
-//       currentPointer = chain(pointer({x: 0, y: 0, preventDefault: false})).start(({ x, y }) => {
-//         if (Math.abs(y) >= AXIS_LOCK_THRESHOLD && !isAxisLocked) {
-//           currentPointer.stop()
-//           upListener.stop()
-//           isAxisLocked = false
-//           return
-//         }
-//         if (Math.abs(x) <= AXIS_LOCK_THRESHOLD) {
-//           return
-//         }
-
-//         window.clickLock = true
-//         isAxisLocked = true
-//         currentPointer.stop()
-
-//         currentPointer = schedule(
-//           everyFrame(),
-//           pointerX(true,
-//             // Set pointer starting point to current position of handleX
-//             getValueFromProgress(
-//               0,
-//               document.body.clientWidth,
-//               Number(handleX.get().replace('%', '')) / 100
-//             ))).pipe(smooth(25), (val) => {
-//           // Convert pixel to percentage value
-//           val = getProgressFromValue(0, document.body.clientWidth, val) * 100
-
-//           return `${val > 0 ? val : 0}%`
-//         }).start(handleX)
-//       })
-
-//       let upListener = listen(element, 'mouseup touchend')
-//         .start((e) => {
-//           if (!isAxisLocked) {
-//             currentPointer.stop()
-//             upListener.stop()
-//             return
-//           }
-
-//           isAxisLocked = false
-//           upListener.stop()
-//           currentPointer.stop()
-
-//           // Everyhthing in percent
-//           let currentPos = Number(`${handleX.get().replace('%', '')}`)
-//           // console.log(currentPos)
-//           let velocity = Number(handleX.getVelocity() / document.body.clientWidth * 100)
-//           let isGoingBack = Boolean(!snap([
-//             0,
-//             47.5
-//           ])(currentPos + velocity))
-//           // let pageWidth = document.body.clientWidth
-
-//           // handleSub = handleX.subscribe(val => {
-//           //   // console.log(val)
-//           //   if (val >= pageWidth) {
-//           //     // console.log('Way to often: ', val)
-//           //     handleSub.unsubscribe()
-//           //     window.clickLock = false
-//           //     stopSpring()
-//           //     window.flamous.location.go('/')
-//           //     // window.flamous.killPage()
-//           //   } else if (val === 0) {
-//           //     handleSub.unsubscribe()
-//           //     window.clickLock = false
-//           //   }
-//           // })
-//           // console.log(isGoingBack)
-//           if (!isGoingBack) {
-//             window.clickLock = true
-//             window.flamous.location.go(window.flamous.getState().location.previous)
-//           } else {
-//             spring({
-//               from: handleX.get(),
-//               to: 0,
-//               damping: 20,
-//               mass: 0.5,
-//               velocity: handleX.getVelocity()
-//             }).pipe(val => { val = Number(val.replace('%', '')); return `${val >= 0 ? val : 0}%` }).start(handleX)
-//           }
-//         })
-//     })
-// }
 const StyledPage = style('article')({
   height: '100%',
   width: '100%',
@@ -180,9 +71,7 @@ const Page = nestable(
             isAxisLocked: false
           }
         }
-        if (Math.abs(x) <= AXIS_LOCK_THRESHOLD) {
-          return
-        }
+        if (Math.abs(x) <= AXIS_LOCK_THRESHOLD) return
 
         window.clickLock = true
         setAxisLock(true)
