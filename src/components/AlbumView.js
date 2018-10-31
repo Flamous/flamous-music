840131557273890359d65fc18b67fc4a8cab740c
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-10-31 19:57:16 +0100

    AlbumView: First draft of new design
    The goa is to make it more distinguishable from the Artist page

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -7,6 +7,16 @@ import kimikoSongs from '../songs/kimiko_ishizaka'
 import SongList from './SongList'
 import { nestable } from 'hyperapp-context'
 
+const style = picostyle(h)
+
+const Img = style('img')({
+  width: '10rem',
+  borderRadius: '2px 1px 1px 2px',
+  borderRight: '2px solid rgba(0, 0, 0, 0.3)',
+  boxShadow: 'rgba(0, 0, 0, 0.3) 0px 4px 25px 1px',
+  transition: 'box-shadow 500ms'
+})
+
 const Album = (props) => {
   let title
   let songs
@@ -28,14 +38,21 @@ const Album = (props) => {
 
   return <div>
     <Header title={title} back={{text: 'Back', to: props.location.previous}}>
-      <HeaderBold style={{textAlign: 'center'}}>
-        <HeaderImage square onclick={(event) => {
+      {/* <HeaderBold style={{textAlign: 'center'}}> */}
+      <div style={{display: 'flex', margin: '1em'}}>
+        <Img src={songs[0].cover_art_url} onclick={(event) => {
           let bounds = event.target.getBoundingClientRect()
           window.flamous.imageViewer.showImageViewer({image: songs[0].cover_art_url, bounds: bounds})
-        }} src={songs[0].cover_art_url} />
-        <p style={{fontSize: '1rem', fontWeight: 'normal', margin: '0.5rem 0 0 -0.2rem', textTransform: 'uppercase'}}>ALBUM</p>
-        {title}
-      </HeaderBold>
+        }} />
+
+        <div style={{marginLeft: '2em'}}>
+
+          <h1>{title}</h1>
+          <p style={{fontSize: '1rem', fontWeight: 'normal', margin: '0.5rem 0 0 -0.2rem'}}>by {songs[0].artist}</p>
+        </div>
+      </div>
+
+      {/* </HeaderBold> */}
     </Header>
     <SongList type='album' albumId={albumId} songs={songs} />
   </div>
