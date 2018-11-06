2beb5167dc739962d3ccc54f1c7cd0e591694da2
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-11-06 11:02:58 +0100

    Home: Remove artists from start page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
 import { h } from 'hyperapp'
 import picostyle from 'picostyle'
-import artists from '../artists.js'
 import Page from './Page.js'
-import Gallery from './Gallery'
 import { Link } from '@hyperapp/router'
 import LazyImage from './LazyImage.js'
 import rightArrow from '../assets/blue_right.svg'
@@ -15,37 +13,6 @@ import twitterImage from '../assets/twitter.svg'
 import githubImage from '../assets/github.svg'
 
 const style = picostyle(h)
-
-const ArtistStyle = style(Link)({
-  textAlign: 'center',
-  width: 'calc(50% - 2em)',
-  border: '1px solid rgba(0, 0, 0, 0.14)',
-  borderRadius: '16px',
-  padding: '1px',
-  margin: '0.7em 1em',
-  boxShadow: '0px 5px 36px -5px rgba(0, 0, 0, 0.14)',
-  '@media (min-width: 800px)': {
-    '&': {
-      maxWidth: '200px'
-    }
-  },
-  '> img': {
-    borderRadius: '16px 16px 3px 3px',
-    width: '100%',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.14)'
-  },
-  ' .artist-line': {
-    color: 'black',
-    fontWeight: 'bold'
-  },
-  ' .secondary': {
-    color: '#212121'
-  },
-  '&:active': {
-    backgroundColor: '#f0f0f0'
-  }
-})
-const Artist = ArtistStyle
 
 const StyledLogo = style('div')({
   display: 'flex',
@@ -142,24 +109,6 @@ const Home = (props) => (context) => {
         <Logo />
         <TagLine />
       </Header>
-      <Gallery id='artists' heading='Browse Artists'>
-        {
-          artists.map((artist, index) => {
-            return <Artist to={`/artists/${artist.name.toLowerCase().replace(' ', '_')}`}>
-              <LazyImage src={artist.cover_art_url} />
-              <div style={{padding: '0.8em 1em 1em'}}>
-                <span class='artist-line'>{artist.name}</span>
-                <br />
-                <span class='secondary'>{artist.songCount} Songs</span>
-              </div>
-            </Artist>
-          })
-        }
-        <p style={{textAlign: 'center', width: '100%'}}>
-          Click on an artist above and listen to some tunes.
-        </p>
-
-      </Gallery>
 
       <Divider />
 
