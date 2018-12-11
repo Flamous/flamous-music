afdd6d89a193899aa38b5477ba1fd33014d3b598
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-11 21:25:38 +0100

    TabBar: Add icons and hover states

--- src/components/UI/UITabBar.js
+++ src/components/UI/UITabBar.js
@@ -3,6 +3,9 @@ import { h } from 'hyperapp'
 import { Link } from '@hyperapp/router'
 import styles from './UITabBar.css'
 import placeholderImage from '~/assets/song_placeholder.svg'
+import homeSVG from '~/assets/home.svg'
+import profileSVG from '~/assets/profile.svg'
+import bookSVG from '~/assets/book.svg'
 
 const UITabBar = (props, children) => (context) => {
   let { actions: { views: { setActive } } } = context
@@ -23,17 +26,17 @@ const UITabBar = (props, children) => (context) => {
     </Link>
 
     <SetActive viewName='home' class={styles['item']}>
-      {/* <img alt='Cover Image' src={placeholderImage} /> */}
+      <img alt='Home Icon' src={homeSVG} />
       <span>Home</span>
     </SetActive>
 
     <SetActive viewName='music-kit' class={styles['item']}>
-      {/* <img alt='Cover Image' src={placeholderImage} /> */}
+      <img alt='Music Kit Icon' src={bookSVG} />
       <span>Music Kit</span>
     </SetActive>
 
     <SetActive viewName='library' class={styles['item']}>
-      {/* <img alt='Cover Image' src={placeholderImage} /> */}
+      <img alt='Library Icon' src={profileSVG} />
       <span>Library</span>
     </SetActive>
   </nav>
