05b5e9e42489163b2f146b61b3329d5a301e7c83
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-11 22:08:49 +0100

    MusicKit: Start creating the start page

--- src/components/MusicKit.js
+++ src/components/MusicKit.js
@@ -2,13 +2,16 @@
 import { h } from 'hyperapp'
 import { Link } from '@hyperapp/router'
 import UIPage from './UI/UIPage'
+import styles from './MusicKit.css'
 
 const MusicKit = () => {
-  return <div class='page'>
-    <h1>Music Kit</h1>
-    <Link to='/artists/123456'>Test 1</Link>
-    <Link to='/albums/654321'>Test 2</Link>
-  </div>
+  return <UIPage nonInteractive>
+    <header class={styles.header}>
+      <span>Curated for startes</span>
+      <h1>Music Kit</h1>
+      <p>Learn how to make music from start to finish.<br />The Music Starter Kit covers a wide variety of topics such as singing, recording and mastering.</p>
+    </header>
+  </UIPage>
 }
 
 export default MusicKit
