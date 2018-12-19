666dd1ed1ae427aab4a4d6a7fc4fba2134d89424
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-19 16:24:15 +0100

    UILink: Add replace property that uses replaceState istead of pushState

--- src/components/UI/UILink.js
+++ src/components/UI/UILink.js
@@ -2,9 +2,9 @@
 import { h } from 'hyperapp'
 
 const UILink = (props, children) => (state) => {
-  let { to } = props
+  let { to, replace } = props
 
-  return <a onclick={(event) => { event.preventDefault(); window.history.pushState({}, '', to) }}>{children}</a>
+  return <a onclick={(event) => { event.preventDefault(); replace ? window.history.replaceState(state.location.previous, '', to) : window.history.pushState(state.location.pathname, '', to) }} {...props}>{children}</a>
 }
 
 export default UILink
