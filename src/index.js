14ff89fb032b857f68ff12fad79f9c1434f18118
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-20 08:43:15 +0100

    Move registerServiceWorker code into its own file

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ import Auth from '@aws-amplify/auth'
 import Login from './components/pages/Login'
 import Home from './components/Home.js'
 import MusicKit from './components/MusicKit'
+import registerServiceWorker from './modules/serviceWorker'
 
 Auth.configure({
   region: 'eu-central-1',
@@ -39,37 +40,7 @@ window.addEventListener('beforeinstallprompt', function (event) {
   window.installPrompt = event
 })
 
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./serviceWorker.js')
-      .then(function (registration) { // Track updates to the Service Worker.
-        if (!navigator.serviceWorker.controller) return
-
-        // When the user asks to refresh the UI, we'll need to reload the window
-        let refreshing
-        navigator.serviceWorker.addEventListener('controllerchange', function (event) {
-          if (refreshing) return
-          refreshing = true
-          window.location.reload()
-        })
-
-        function listenInstalledStateChange () {
-          registration.installing.addEventListener('statechange', function (event) {
-            if (event.target.state === 'installed') {
-              // A new service worker is available, inform the user
-              window.flamous.updateAvailable(registration)
-            }
-          })
-        }
-        if (registration.waiting) return window.flamous.updateAvailable(registration)
-
-        if (registration.installing) return listenInstalledStateChange()
-
-        registration.addEventListener('updatefound', listenInstalledStateChange)
-      })
-  })
-}
+registerServiceWorker()
 
 const style = picostyle(h)
 
