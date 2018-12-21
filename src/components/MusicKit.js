7c1fa7f0ae4c0424c31a158102a34c5175ec81d1
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-21 20:30:23 +0100

    MusicKit: Add some more styling and initial mockups

--- src/components/MusicKit.js
+++ src/components/MusicKit.js
@@ -3,15 +3,47 @@ import { h } from 'hyperapp'
 import { Link } from '@hyperapp/router'
 import UIPage from './UI/UIPage'
 import styles from './MusicKit.css'
+import UIHeader from './UI/UIHeader'
 
 const MusicKit = () => {
-  return <UIPage nonInteractive>
-    <header class={styles.header}>
-      <span>Curated for startes</span>
-      <h1>Music Kit</h1>
-      <p>Learn how to make music from start to finish.<br />The Music Starter Kit covers a wide variety of topics such as singing, recording and mastering.</p>
-    </header>
+  return <UIPage class={styles['music-kit']} nonInteractive>
+    <UIHeader title='Music Kit' />
+
+    <main class={styles['main']}>
+      <div class={styles['wrapper']}>
+        <section>
+          <p>Learn how to make music from start to finish. The Music Kit covers a wide variety of topics such as singing, recording and mastering.</p>
+          <p>
+      Making and sharing your own music is something that has fascinated cultures all over the world with its magic.
+          </p>
+        </section>
+
+        <section>
+          <h2>
+          Topics
+          </h2>
+          <Card
+            title='Explore Your Voice'
+          />
+          <Card
+            title='Record'
+          />
+          <Card
+            title='Publish'
+          />
+        </section>
+      </div>
+    </main>
   </UIPage>
 }
 
 export default MusicKit
+
+const Card = (props) => {
+  let { bgImage, title } = props
+
+  return <div class={styles['card']}>
+    <h2>{title}</h2>
+    <img src={bgImage} />
+  </div>
+}
