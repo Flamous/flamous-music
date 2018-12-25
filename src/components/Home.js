aa335cdfb4d5b8ea3f473d7bfe6432728606a8fe
Christian Kaindl <crisscross.kaindl@outlook.de>
2018-12-25 19:27:58 +0100

    Home: Use UIPage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 /** @jsx h */
 import { h } from 'hyperapp'
-import Page from './Page.js'
+import UIPage from './UI/UIPage.js'
 import UIHeader from './UI/UIHeader.js'
 
 const Home = (props) => (context) => {
   // if (props.match.url === '/' && context.initialLoad) window.flamous.setInitialLoad(false)
   return (
-    <Page nonInteractive key='home'>
+    <UIPage nonInteractive key='home'>
       <UIHeader title='Home' />
-    </Page>
+    </UIPage>
   )
 }
 
