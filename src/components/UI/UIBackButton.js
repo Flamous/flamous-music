0f7c2e87b5cbb97cd528d622969f489693fd077d
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-01-05 22:26:12 +0100

    UIBackButton: Add some styling

--- src/components/UI/UIBackButton.js
+++ src/components/UI/UIBackButton.js
@@ -2,6 +2,7 @@
 import { h } from 'hyperapp'
 import UILink from './UILink'
 import leftArrow from '~/assets/blue_left.svg'
+import styles from './UIBackButton.css'
 
 export default (props, children) => (state) => {
   let { text } = props
@@ -12,7 +13,7 @@ export default (props, children) => (state) => {
   let backLocation = previousViewStackPath || `/${views.activeView}`
   let isBrowserHistoryBack = location.previous === previousViewStackPath
 
-  return <UILink style={{ fontWeight: 'bold' }} back={isBrowserHistoryBack} replace={!isBrowserHistoryBack} to={backLocation}>
-    <img src={leftArrow} /> {text || 'Back'}
+  return <UILink class={styles['back-button']} back={isBrowserHistoryBack} replace={!isBrowserHistoryBack} to={backLocation}>
+    <img src={leftArrow} /><span>{text || 'Back'}</span>
   </UILink>
 }
