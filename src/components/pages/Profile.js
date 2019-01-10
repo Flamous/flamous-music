b7041b37141fc414a84a3afb7b723949bf2ac02f
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-01-10 18:04:24 +0100

    Profile: Add rich album cards w/ description and cover image

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,9 +5,12 @@ import UILink from '../UI/UILink'
 import UIHeader from '../UI/UIHeader'
 import UISpinner from '../UI/UISpinner'
 import UIBackButton from '../UI/UIBackButton'
+import styles from './Profile.css'
+import placeholderAlbum from '~/assets/song_placeholder.svg'
 
 const Library = (props) => (context, actions) => {
   let { auth, actions: { auth: { logout } } } = context
+  let isAlbums = auth.albums && Object.keys(auth.albums).length
 
   return <UIPage {...props}>
     <UIHeader
@@ -15,37 +18,43 @@ const Library = (props) => (context, actions) => {
       nav={{ start: <UIBackButton /> }}
     />
 
-    <div style={{ textAlign: 'center' }}>
-      <p>
-        {
-          !auth.isAuthenticated && <main>
+    <div>
+
+      {
+        !auth.isAuthenticated && <main>
+          <p style={{ textAlign: 'center' }}>
             Share your music with the world. <br />Sign Up to upload music.
             <br />
             <br />
             <UILink class='button' to='/signup'>Create Account</UILink>
             <UILink class='button white' to='login'>or Sign In</UILink>
-          </main>
-        }
-      </p>
+          </p>
+        </main>
+      }
 
       {
-        auth.isAuthenticated && <div>
-          <div>
-              Logged in as<br />{auth.cognitoUser.attributes.email}
-            <p>
-              <button onclick={logout}>Logout</button>
-            </p>
-          </div>
-
+        auth.isAuthenticated && <main class={styles['main']}>
           <section>
             <h3>Your Albums</h3>
             {
-              auth.albums && <div>
+              isAlbums && <div class={styles['album-list']}>
                 {
                   auth.albums.map((album) => {
-                    return <div>
-                      <UILink to={`/albums/${album.albumId}`}>{album.title}</UILink>
-                    </div>
+                    return <UILink class={styles['album']} to={`/albums/${album.albumId}`}>
+                      <div class={styles['image-wrapper']}>
+                        <img src={album.coverImagePath ? `${auth.s3BasePath}/albums/${album.albumId}/cover` : placeholderAlbum} />
+                      </div>
+                      <div class={styles['text-wrapper']}>
+                        <div>
+                          <h2>{album.title}</h2>
+                          <p>{album.description || 'No description.'}</p>
+                        </div>
+                        <div class={styles['row-bottom']}>
+                          <button class='white'>Edit</button>
+                          <span>10 Songs</span>
+                        </div>
+                      </div>
+                    </UILink>
                   })
                 }
               </div>
@@ -58,18 +67,25 @@ const Library = (props) => (context, actions) => {
               </div>
             }
             {
-              auth.isLoadingAlbums && <UISpinner />
+              (auth.isLoadingAlbums || auth.isLoadingUser) && <UISpinner />
             }
-
-          </section>
-
-          <section>
-            <p>
+            <div>
+              <br />
               <UILink to='create-album' class='button'>Create New Album</UILink>
-            </p>
+            </div>
           </section>
-        </div>
 
+          <section style={{ borderTop: '1px solid rgba(0, 0, 0, 0.2)' }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flexGrow: '1' }}>
+            Logged in as<br /><b>{auth.cognitoUser.attributes.email}</b>
+              </div>
+              <div>
+                <button onclick={logout}>Logout</button>
+              </div>
+            </div>
+          </section>
+        </main>
       }
     </div>
   </UIPage>
