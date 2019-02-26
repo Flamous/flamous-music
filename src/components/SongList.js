15c7e9e3539973633a498a30f21d4f98c3c2cd77
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-26 20:21:14 +0100

    New component: SongList (initial draft)
    Displays a list of songs, together with a cover image and a action menu

---
+++ src/components/SongList.js
@@ -0,0 +1,65 @@
+/** @jsx h */
+import { h } from 'hyperapp'
+import styles from './SongList.css'
+import albumCoverPlaceholer from '../assets/song_placeholder.svg'
+import UIIcon from './UI/UIIcon'
+import UILink from './UI/UILink'
+
+let view = (props, children) => (state) => (context) => {
+  let { songs = [] } = props
+  let { actionMenu } = context.actions
+
+  return <ul class={styles['song-list']}>
+    { songs.length > 0 && songs.map(song => (
+      <li>
+        <UILink class={styles['song-item']} to='/'>
+          <div class={styles['song-body']}>
+            <div>
+              <span class={styles['song-title']}>
+                {song.title}
+              </span>
+              <br />
+              <span class={styles['song-infos']}>
+                {song.artist} &middot; {song.album}
+              </span>
+            </div>
+            <button class='white' onclick={event => {
+              actionMenu.open({
+                event,
+                items: [
+                  { text: 'Add to Queue',
+                    icon: 'corner-down-left'
+                  },
+                  { text: 'Save to Library',
+                    icon: 'heart'
+                  },
+                  { text: 'Go to Album',
+                    icon: 'disc'
+                  },
+                  { text: 'Go to Artist',
+                    icon: 'user'
+                  },
+                  { text: 'Share',
+                    icon: 'share-2'
+                  },
+                  { text: 'Download Audio File',
+                    icon: 'download'
+                  }
+                ]
+              })
+            }}>
+              <UIIcon icon='more-horizontal' />
+            </button>
+          </div>
+          <div class={styles['cover-image']}>
+            <img src={albumCoverPlaceholer} />
+          </div>
+        </UILink>
+      </li>
+    ))}
+  </ul>
+}
+
+let SongList = view
+
+export default SongList
