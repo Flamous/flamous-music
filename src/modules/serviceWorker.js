cca4f95eb7d601d11e20b662782b11907c6ddb6a
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-28 00:01:20 +0100

    serviceWorker.js: Remove complicated update logic
    Also add error handler so we get to know if an error occured

--- src/modules/serviceWorker.js
+++ src/modules/serviceWorker.js
@@ -4,30 +4,33 @@ function registerServiceWorker () {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/serviceWorker.js')
         .then(function (registration) { // Track updates to the Service Worker.
-          if (!navigator.serviceWorker.controller) return
+          // if (!navigator.serviceWorker.controller) return
 
-          // When the user asks to refresh the UI, we'll need to reload the window
-          let refreshing
-          navigator.serviceWorker.addEventListener('controllerchange', function (event) {
-            if (refreshing) return
-            refreshing = true
-            window.location.reload()
-          })
+          // // When the user asks to refresh the UI, we'll need to reload the window
+          // let refreshing
+          // navigator.serviceWorker.addEventListener('controllerchange', function (event) {
+          //   if (refreshing) return
+          //   refreshing = true
+          //   window.location.reload()
+          // })
 
-          function listenInstalledStateChange () {
-            registration.installing.addEventListener('statechange', function (event) {
-              if (event.target.state === 'installed') {
-                // A new service worker is available, inform the user
-                window.flamous.updateAvailable(registration)
-              }
-            })
-          }
-          if (registration.waiting) return window.flamous.updateAvailable(registration)
+          // function listenInstalledStateChange () {
+          //   registration.installing.addEventListener('statechange', function (event) {
+          //     if (event.target.state === 'installed') {
+          //       // A new service worker is available, inform the user
+          //       // window.flamous.updateAvailable(registration)
+          //     }
+          //   })
+          // }
+          // if (registration.waiting) return window.flamous.updateAvailable(registration)
 
-          if (registration.installing) return listenInstalledStateChange()
+          // if (registration.installing) return listenInstalledStateChange()
 
-          registration.addEventListener('updatefound', listenInstalledStateChange)
+          // registration.addEventListener('updatefound', listenInstalledStateChange)
+
+          console.info('registered service worker')
         })
+        .catch(console.error)
     })
   }
 }
