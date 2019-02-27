2d480dcbcc355b056f02b763c303a6091ed4fcf2
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-27 18:46:36 +0100

    Home: Display new AlbumList component with some dummy data

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,35 +4,36 @@ import UIPage from './UI/UIPage.js'
 import UILink from './UI/UILink'
 import styles from './Home.css'
 import logo from '~/assets/flamous_logo_new_small.svg'
-import dummyPlaceholder from '~/assets/dummy_artists.jpg'
-import musicKitIcon from '~/assets/icons/kit_blue.svg'
+// import dummyPlaceholder from '~/assets/dummy_artists.jpg'
+// import musicKitIcon from '~/assets/icons/kit_blue.svg'
 import UIHeader from './UI/UIHeader.js'
 import SongList from './SongList'
+import AlbumList from './AlbumList'
 
-const Header = () => (state) => {
-  let { auth: { isAuthenticated: isSignedIn } } = state
-  return <header class={styles['header']}>
-    <div class={styles['inner']}>
-      <img src={logo} />
-      <h1>
-      Music for<br />Everyone
-      </h1>
-      <p>
-      Free (<UILink to='/license'>listen-like-you-can</UILink>) music. You can use them for commercial and noncommercial purposes.
-      </p>
-      {
-        !isSignedIn && <nav>
-          <div>
-            <UILink class='button' to='/signup'>Create Account</UILink>
-          </div>
-          <div>
-            <UILink class='button white' to='/login'>or Sign In</UILink>
-          </div>
-        </nav>
-      }
-    </div>
-  </header>
-}
+// const Header = () => (state) => {
+//   let { auth: { isAuthenticated: isSignedIn } } = state
+//   return <header class={styles['header']}>
+//     <div class={styles['inner']}>
+//       <img src={logo} />
+//       <h1>
+//       Music for<br />Everyone
+//       </h1>
+//       <p>
+//       Free (<UILink to='/license'>listen-like-you-can</UILink>) music. You can use them for commercial and noncommercial purposes.
+//       </p>
+//       {
+//         !isSignedIn && <nav>
+//           <div>
+//             <UILink class='button' to='/signup'>Create Account</UILink>
+//           </div>
+//           <div>
+//             <UILink class='button white' to='/login'>or Sign In</UILink>
+//           </div>
+//         </nav>
+//       }
+//     </div>
+//   </header>
+// }
 
 let songData = [
   {
@@ -59,6 +60,34 @@ let songData = [
     title: 'Easy [iPhone Production]',
     artist: 'Wowa',
     album: 'Single'
+  }
+]
+
+let albumData = [
+  {
+    title: 'Goldberg Variations',
+    artist: 'Kimiko Ishizaka',
+    songCount: 25
+  },
+  {
+    title: 'adfs',
+    artist: 'sdfadsf',
+    songCount: 5
+  },
+  {
+    title: 'The Art of the Fuge',
+    artist: 'Kimiko Ishizaka',
+    songCount: 5
+  },
+  {
+    title: 'adfs',
+    artist: 'sdfadsf',
+    songCount: 5
+  },
+  {
+    title: 'adfs',
+    artist: 'sdfadsf',
+    songCount: 5
   }
 ]
 
@@ -94,7 +123,7 @@ const Home = (props) => (context) => {
           <h2>
             Random Albums
           </h2>
-          <SongList songs={songData} />
+          <AlbumList albums={albumData} />
         </section>
       </main>
       <footer>
