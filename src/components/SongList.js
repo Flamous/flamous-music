6eeb10eeab0dda8b5e9133caa918bc67ffae649f
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-28 00:06:02 +0100

    SongList: Open actionMenu on long-press

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -9,10 +9,45 @@ let view = (props, children) => (state) => (context) => {
   let { songs = [] } = props
   let { actionMenu } = context.actions
 
+  function openActionMenu (event) {
+    actionMenu.open({
+      event,
+      items: [
+        { text: 'Add to Queue',
+          icon: 'corner-down-left'
+        },
+        { text: 'Save to Library',
+          icon: 'heart'
+        },
+        { text: 'Go to Album',
+          icon: 'disc'
+        },
+        { text: 'Go to Artist',
+          icon: 'user'
+        },
+        { text: 'Share',
+          icon: 'share-2'
+        },
+        { text: 'Download Audio File',
+          icon: 'download'
+        },
+        { text: 'Report Content',
+          icon: 'slash'
+        }
+      ]
+    })
+  }
+
   return <ul class={styles['song-list']}>
     { songs.length > 0 && songs.map(song => (
       <li>
-        <UILink class={styles['song-item']} to='/'>
+        <UILink
+          class={styles['song-item']}
+          to='/'
+          oncreate={elem => {
+            elem.addEventListener('long-press', openActionMenu)
+          }}
+        >
           <div class={styles['song-body']}>
             <div class={styles['song-text']}>
               <span class={styles['song-title']}>
@@ -23,34 +58,7 @@ let view = (props, children) => (state) => (context) => {
                 {song.artist} &middot; {song.album}
               </span>
             </div>
-            <button class='white' onclick={event => {
-              actionMenu.open({
-                event,
-                items: [
-                  { text: 'Add to Queue',
-                    icon: 'corner-down-left'
-                  },
-                  { text: 'Save to Library',
-                    icon: 'heart'
-                  },
-                  { text: 'Go to Album',
-                    icon: 'disc'
-                  },
-                  { text: 'Go to Artist',
-                    icon: 'user'
-                  },
-                  { text: 'Share',
-                    icon: 'share-2'
-                  },
-                  { text: 'Download Audio File',
-                    icon: 'download'
-                  },
-                  { text: 'Report Content',
-                    icon: 'slash'
-                  }
-                ]
-              })
-            }}>
+            <button class='white' onclick={openActionMenu}>
               <UIIcon icon='more-horizontal' />
             </button>
           </div>
