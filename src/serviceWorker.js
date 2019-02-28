47b88d28bf7cfdc9722594ef3f9981505db71d8c
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-28 14:17:14 +0100

    Update serviceWorker to 4.0.0

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,9 +1,5 @@
 /* eslint-env serviceworker */
 /* global workbox */
 
-self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js')
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js')
 workbox.precaching.precacheAndRoute([])
-
-self.addEventListener('message', messageEvent => {
-  if (messageEvent.data === 'skipWaiting') return skipWaiting()
-})
