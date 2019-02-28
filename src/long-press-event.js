47c61effc23fc515850dbd8314c6c889e47f0471
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-02-28 11:37:18 +0100

    Rewrite long-press logic to fit our use-case better

--- src/long-press-event.js
+++ src/long-press-event.js
@@ -1,91 +1,27 @@
-/*!
- * long-press-event.js
- * Pure JavaScript long-press-event
- * https://github.com/john-doherty/long-press-event
- * @author John Doherty <www.johndoherty.info>
- * @license MIT
- */
-'use strict'
+/* Originally inspired by John Doherty's long-press-event: https://github.com/john-doherty/long-press-event */
 
-var timer = null
+let timer = null
 
-// check if we're using a touch screen
-var isTouch = (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0))
+function cancelLongPress (event) {
+  clearTimeout(timer)
+}
 
-// switch to touch events if using a touch screen
-var mouseDown = isTouch ? 'touchstart' : 'mousedown'
-var mouseOut = isTouch ? 'touchcancel' : 'mouseout'
-var mouseUp = isTouch ? 'touchend' : 'mouseup'
-var mouseMove = isTouch ? 'touchmove' : 'mousemove'
-
-// wheel/scroll events
-var mouseWheel = 'mousewheel'
-var wheel = 'wheel'
-var scrollEvent = 'scroll'
-
-// patch CustomEvent to allow constructor creation (IE/Chrome)
-if (typeof window.CustomEvent !== 'function') {
-  window.CustomEvent = function (event, params) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined }
-
-    var evt = document.createEvent('CustomEvent')
-    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
-    return evt
-  }
-
-  window.CustomEvent.prototype = window.Event.prototype
+function fireLongPressEvent (event) {
+  this.dispatchEvent(new window.CustomEvent('long-press', { bubbles: true, cancelable: true }))
+  cancelLongPress()
 }
 
 // listen to mousedown event on any child element of the body
-document.addEventListener(mouseDown, function (e) {
-  var el = e.target
+document.addEventListener('touchstart', function (event) {
+  let el = event.target
+  el.oncontextmenu = event => {
+    event.preventDefault()
+    return false
+  }
 
-  // get delay from html attribute if it exists, otherwise default to 1500
-  var longPressDelayInMs = parseInt(el.getAttribute('data-long-press-delay') || '500', 10)
-
-  // start the timer
-  timer = setTimeout(fireLongPressEvent.bind(el), longPressDelayInMs)
+  timer = setTimeout(fireLongPressEvent.bind(el), 500)
 })
 
-// clear the timeout if the user releases the mouse/touch
-document.addEventListener(mouseUp, function (e) {
-  clearTimeout(timer)
-})
-
-// clear the timeout if the user leaves the element
-document.addEventListener(mouseOut, function (e) {
-  clearTimeout(timer)
-})
-
-// clear if the mouse moves
-document.addEventListener(mouseMove, function (e) {
-  clearTimeout(timer)
-})
-
-// clear if the Wheel event is fired in the element
-document.addEventListener(mouseWheel, function (e) {
-  clearTimeout(timer)
-})
-
-// clear if the Scroll event is fired in the element
-document.addEventListener(wheel, function (e) {
-  clearTimeout(timer)
-})
-
-// clear if the Scroll event is fired in the element
-document.addEventListener(scrollEvent, function (e) {
-  clearTimeout(timer)
-})
-
-/**
-   * Fires the 'long-press' event on element
-   * @returns {void}
-   */
-function fireLongPressEvent () {
-  // fire the long-press event
-  this.dispatchEvent(new CustomEvent('long-press', { bubbles: true, cancelable: true }))
-
-  clearTimeout(timer)
-
-  if (console && console.log) console.log('long-press event fired on ' + this.outerHTML)
-}
+document.addEventListener('touchend', cancelLongPress)
+document.addEventListener('touchcancel', cancelLongPress)
+document.addEventListener('touchmove', cancelLongPress)
