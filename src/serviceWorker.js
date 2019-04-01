3899f6b569faabef393d9b0afe35076172f9fcfd
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-04-01 16:26:24 +0200

    serviceWorker - Cache polyfill

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -3,3 +3,8 @@
 
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js')
 workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  new RegExp('https://cdn.polyfill.io/.*'),
+  new workbox.strategies.StaleWhileRevalidate()
+)
