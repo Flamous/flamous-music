e197e98ca74735b9c054edef73fd46b54f27befc
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-04-06 17:50:33 +0200

    Home - Add bold view title "Explore"

--- src/components/Home.js
+++ src/components/Home.js
@@ -101,8 +101,6 @@ const Home = (props) => (context) => {
         description='Free high-quality music by our amazing community.'
       />
       <UIHeader
-        noDynamicTitle
-        noSticky
         nav={{
           start: <img class={styles['logo']} src={logo} />,
           end: <div>
@@ -110,6 +108,7 @@ const Home = (props) => (context) => {
             <UILink to='/signup' class='button white' style={{ fontWeight: '500' }}>Sign Up</UILink>
           </div>
         }}
+        title='Explore'
       />
       <main class={styles['main']}>
         <section>
