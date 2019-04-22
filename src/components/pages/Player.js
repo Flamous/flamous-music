28d25f04ce280be2ebaab6bd8c5b41367b7b3a7a
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-04-22 11:41:15 +0200

    Player - Make responsive fixes for desktop

--- src/components/pages/Player.js
+++ src/components/pages/Player.js
@@ -56,84 +56,86 @@ const view = (state, actions) => (props) => (context) => {
       }
     }}
   >
-    <header class={styles['header']}>
-      <UILink back class={styles['close']}>
-        <UIIcon height='48' width='48' icon='chevron-down' />
-      </UILink>
-      <h1>
+    <div class={styles['wrapper']}>
+      <header class={styles['header']}>
+        <UILink back class={styles['close']}>
+          <UIIcon height='48' width='48' icon='chevron-down' />
+        </UILink>
+        <h1>
         Playing
-      </h1>
-      <span>from [Playlist]</span>
-    </header>
+        </h1>
+        {/* <span>from [Playlist]</span> */}
+      </header>
 
-    <main class={styles['main']}>
-      <div class={styles['cover-image']}>
-        <img src={placeholder} />
-      </div>
-      <div class={styles['song-infos']}>
-        <span class={styles['title']}>Song Title</span><br />
-        <span class={styles['artists']}>Artists 1, Artist 2</span>
-      </div>
-      <div class={styles['scrubber']}>
-        <span>
-          00:00
-        </span>
-        <div class={styles['slider']}>
-          <span class={styles['thumb']} />
+      <main class={styles['main']}>
+        <div class={styles['cover-image']}>
+          <img src={placeholder} />
         </div>
-        <span>
+        <div class={styles['song-infos']}>
+          <span class={styles['title']}>Song Title</span><br />
+          <span class={styles['artists']}>Artists 1, Artist 2</span>
+        </div>
+        <div class={styles['scrubber']}>
+          <span>
           00:00
-        </span>
-      </div>
-      <div class={styles['controls']}>
-        <button class='white'>
-          <UIIcon height='36' width='36' icon='rewind' />
-        </button>
-        <button class={cc(['white', styles['play']])}>
-          <UIIcon height='48' width='48' icon='pause' />
-        </button>
-        <button class='white'>
-          <UIIcon height='36' width='36' icon='fast-forward' />
-        </button>
-      </div>
-    </main>
+          </span>
+          <div class={styles['slider']}>
+            <span class={styles['thumb']} />
+          </div>
+          <span>
+          00:00
+          </span>
+        </div>
+        <div class={styles['controls']}>
+          <button class='white'>
+            <UIIcon height='36' width='36' icon='rewind' />
+          </button>
+          <button class={cc(['white', styles['play']])}>
+            <UIIcon height='48' width='48' icon='pause' />
+          </button>
+          <button class='white'>
+            <UIIcon height='36' width='36' icon='fast-forward' />
+          </button>
+        </div>
+      </main>
 
-    <footer class={styles['footer']}>
-      <button class='white'>
-        <UIIcon icon='shuffle' />
-      </button>
-      <button class='white'>
-        <UIIcon height='30' width='30' icon='plus' />
-        <span>Add to Library</span>
-      </button>
-      {/* <button class='white'>
+      <footer class={styles['footer']}>
+        <button class='white'>
+          <UIIcon icon='shuffle' />
+        </button>
+        <button class='white'>
+          <UIIcon height='30' width='30' icon='plus' />
+          <span>Add to Library</span>
+        </button>
+        {/* <button class='white'>
           <UIIcon icon='share-2' />
         </button> */}
-      <button
-        class='white'
-        onclick={(event) => context.actions.actionMenu.open({
-          items: [
-            { text: 'Share',
-              icon: 'share-2'
-            },
-            { text: 'Loop Song',
-              icon: 'repeat'
-            },
-            { text: 'Go to Artist',
-              icon: 'user'
-            },
-            { text: 'Download Audio File',
-              icon: 'download'
-            },
-            { text: 'Report Content',
-              icon: 'slash'
-            }
-          ],
-          event
-        })}>
-        <UIIcon height='30' width='30' icon='more-horizontal' />
-      </button>
-    </footer>
+        <button
+          class='white'
+          onclick={(event) => context.actions.actionMenu.open({
+            items: [
+              { text: 'Share',
+                icon: 'share-2'
+              },
+              { text: 'Loop Song',
+                icon: 'repeat'
+              },
+              { text: 'Go to Artist',
+                icon: 'user'
+              },
+              { text: 'Download Audio File',
+                icon: 'download'
+              },
+              { text: 'Report Content',
+                icon: 'slash'
+              }
+            ],
+            event
+          })}>
+          <UIIcon height='30' width='30' icon='more-horizontal' />
+        </button>
+      </footer>
+    </div>
   </div>
 }
 
