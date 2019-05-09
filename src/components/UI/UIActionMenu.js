e3a0b47a4da188ac21d07b575cedc745536cf382
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-05-09 16:17:25 +0200

    UIActionMenu - Make it work with touch and cursor
    On devices with cursors, the context menu is not a fullscreen overlay, but rather a local menu.

--- src/components/UI/UIActionMenu.js
+++ src/components/UI/UIActionMenu.js
@@ -10,22 +10,53 @@ let state = {
 }
 
 let actions = {
-  animation: slideUp.actions
+  animation: slideUp.actions,
+  setMenuPos: (pos) => {
+    let { x, y } = pos
+    let width = 342 // Measured with DevTools ¯\_(ツ)_/¯
+    let height = 360 // Also measured with DevTools
+    let xMargin = window.innerWidth - (x + width)
+    let yMargin = window.innerHeight - (y + height)
+
+    if (xMargin < 0) x += xMargin
+    if (yMargin < 0) y += yMargin
+
+    return {
+      x,
+      y
+    }
+  }
 }
 
 let view = (state, actions) => (props, children) => (context) => {
-  let { items } = props
+  let { items, details } = props
   let { close } = context.actions.actionMenu
   let { start: startAnimation } = actions.animation
+  let { x, y } = state
 
   return <div
-    oncreate={(elem) => { elem.parentNode.actions = actions; window.requestAnimationFrame(() => window.requestAnimationFrame(() => elem.parentNode.classList.toggle('open'))) }}
+    oncreate={(elem) => {
+      details.pos && actions.setMenuPos({ x: details.pos.x, y: details.pos.y })
+      elem.parentNode.actions = actions
+      window.requestAnimationFrame(() => window.requestAnimationFrame(() => elem.parentNode.classList.toggle('open')))
+
+      if (details.initiator === 'touch') {
+        elem.classList.add(styles['touch'])
+      }
+      if (details.initiator === 'cursor') {
+        elem.classList.add(styles['cursor'])
+      }
+    }}
   >
     <div
       class={styles['background']}
       onclick={close}
     />
-    <div class={styles['container']} oncreate={(element) => startAnimation({ element, initialLoad: context.initialLoad })}>
+    <div
+      style={x && y ? `transform: translateX(${x - 5}px) translateY(${y - 5}px)` : details.initiator === 'touch' ? '' : 'display: none;'}
+      class={styles['container']}
+      oncreate={(element) => details.initiator === 'touch' && startAnimation({ element, initialLoad: context.initialLoad })}
+    >
       <ul class={styles['action-list']}>
         {
           items && items.map((item) => {
@@ -61,7 +92,16 @@ const UIActionMenu = nestable(
 )
 export default (props) => (
   <UIActionMenu
-    onremove={(elem, done) => { elem.classList.toggle('open'); elem.actions.animation.slideOut({ done, elem }) }}
+    onremove={(elem, done) => {
+      let initiator = props.details.initiator
+      elem.classList.toggle('open')
+      if (initiator === 'touch') {
+        elem.actions.animation.slideOut({ done, elem })
+        return
+      }
+
+      done()
+    }}
     {...props}
     key='action-menu'
     class={styles['action-menu']}
