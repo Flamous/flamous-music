6cd2b3da2a0c8b7fa57725490a53e7ea1801f02d
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-05-30 18:04:42 +0200

    gqlApi.js- Fix missing import

--- src/components/functions/gqlApi.js
+++ src/components/functions/gqlApi.js
@@ -1,4 +1,4 @@
-import API from '@aws-amplify/api'
+import API, { graphqlOperation } from '@aws-amplify/api'
 
 export default function gqlApi (options) {
   console.info('Flamous: GraphQL action -->', options)
