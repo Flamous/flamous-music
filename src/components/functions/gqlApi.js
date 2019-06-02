9ad52d155a02e7f98d432d2a50ee7c616f28fc0f
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-06-02 15:23:59 +0200

    gqlApi - Add authMode to calls

--- src/components/functions/gqlApi.js
+++ src/components/functions/gqlApi.js
@@ -2,9 +2,10 @@ import API, { graphqlOperation } from '@aws-amplify/api'
 
 export default function gqlApi (options) {
   console.info('Flamous: GraphQL action -->', options)
-  let { operation, parameters = {} } = options
+  let { operation, parameters = {}, authMode = 'AMAZON_COGNITO_USER_POOLS' } = options
   return new Promise(function (resolve, reject) {
-    API.graphql(graphqlOperation(operation, parameters))
+    console.log("Making call with authMode ", authMode)
+    API.graphql(graphqlOperation(operation, parameters), authMode)
     .then((response) => {
       let unwrappedData = response.data[Object.keys(response.data)[0]]
       resolve(unwrappedData)
