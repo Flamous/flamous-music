b503d4f10cabb50a052cb5957ebf7e24cd85d0f8
Christian Kaindl <crisscross.kaindl@outlook.de>
2019-06-05 01:27:33 +0200

    SongList - Add boilerplate and make Go to Album funtional

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,58 +4,68 @@ import styles from './SongList.css'
 import albumCoverPlaceholer from '../assets/song_placeholder.svg'
 import UIIcon from './UI/UIIcon'
 
-let ctx = {
-  standalone: [
-    { text: 'Add to Queue',
-      icon: 'corner-down-left'
-    },
-    { text: 'Save to Library',
-      icon: 'heart'
-    },
-    { text: 'Go to Album',
-      icon: 'disc'
-    },
-    { text: 'Go to Artist',
-      icon: 'user'
-    },
-    { text: 'Share Song',
-      icon: 'share-2'
-    },
-    { text: 'Download Audio File',
-      icon: 'download'
-    },
-    { text: 'Report Content',
-      icon: 'slash'
-    }
-  ],
-  album: [
-    { text: 'Save to Library',
-      icon: 'heart'
-    },
-    { text: 'Add to Queue',
-      icon: 'corner-down-left'
-    },
-    { text: 'Go to Artist',
-      icon: 'user'
-    },
-    { text: 'Share Song',
-      icon: 'share-2'
-    }
-  ],
-  artist: [
-    { text: 'Save to Library',
-      icon: 'heart'
-    },
-    { text: 'Add to Queue',
-      icon: 'corner-down-left'
-    },
-    { text: 'Go to Album',
-      icon: 'disc'
-    },
-    { text: 'Share Song',
-      icon: 'share-2'
-    }
-  ]
+function buildActions (song) {
+  let { aritstId, albumId } = song
+
+  let ctx = {
+    standalone: [
+      { text: 'Add to Queue',
+        icon: 'corner-down-left'
+      },
+      { text: 'Save to Library',
+        icon: 'heart'
+      },
+      { text: 'Go to Album',
+        icon: 'disc',
+        action: function goToArtist({ close }) {
+          window.history.pushState({}, '', `/albums/${albumId}`)
+          close()
+        }
+      },
+      { text: 'Go to Artist',
+        icon: 'user'
+      },
+      { text: 'Share Song',
+        icon: 'share-2'
+      },
+      { text: 'Download Audio File',
+        icon: 'download'
+      },
+      { text: 'Report Content',
+        icon: 'slash'
+      }
+    ],
+    album: [
+      { text: 'Save to Library',
+        icon: 'heart'
+      },
+      { text: 'Add to Queue',
+        icon: 'corner-down-left'
+      },
+      { text: 'Go to Artist',
+        icon: 'user'
+      },
+      { text: 'Share Song',
+        icon: 'share-2'
+      }
+    ],
+    artist: [
+      { text: 'Save to Library',
+        icon: 'heart'
+      },
+      { text: 'Add to Queue',
+        icon: 'corner-down-left'
+      },
+      { text: 'Go to Album',
+        icon: 'disc'
+      },
+      { text: 'Share Song',
+        icon: 'share-2'
+      }
+    ]
+  }
+
+  return ctx
 }
 
 /**
@@ -69,8 +79,8 @@ let view = (props, children) => (state) => (context) => {
   let { songs = [], mode = 'standalone', album } = props
   let { actionMenu } = context.actions
 
-  // openActionMenu should be extracted (probably into a decorator). Maybe something along CtxMenuAction
-  function openActionMenu (event, initiator) {
+  // openActionMenu should be extracted (probably into a decorator). Maybe something along CtxMenuAction?
+  function openActionMenu (event, initiator, song) {
     let details = event.detail
 
     if (!details.initiator) {
@@ -88,7 +98,7 @@ let view = (props, children) => (state) => (context) => {
     actionMenu.open({
       event,
       details,
-      items: ctx[mode]
+      items: buildActions(song)[mode]
     })
   }
 
@@ -101,7 +111,7 @@ let view = (props, children) => (state) => (context) => {
           class={styles['song-item']}
           to='/'
           oncreate={elem => {
-            elem.addEventListener('long-press', openActionMenu)
+            elem.addEventListener('long-press', (event) => openActionMenu(event, null, song))
           }}
         >
           <div class={styles['song-body']}>
@@ -114,7 +124,7 @@ let view = (props, children) => (state) => (context) => {
                 {song.artist} {mode !== 'album' ? <span>&middot; {album}</span> : ''}
               </span>
             </div>
-            <button class='white' onmousedown={(e) => openActionMenu(e, 'cursor')} onclick={(e) => !actionMenuIsOpen && openActionMenu(e, 'touch')}>
+            <button class='white' onmousedown={(e) => openActionMenu(e, 'cursor', song)} onclick={(e) => !actionMenuIsOpen && openActionMenu(e, 'touch', song)}>
               <UIIcon icon='more-horizontal' />
             </button>
           </div>
